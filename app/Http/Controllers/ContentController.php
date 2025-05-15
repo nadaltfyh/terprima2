bf2d8dcda33d6ebbe020d009260fe84a7f9415bb
nadaltfyh <nadalutfiyah1@gmail.com>
2025-05-15 14:36:28 +0700

    media1

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\content;
+use App\Models\media;
 use Illuminate\Support\Facades\Storage;
 
 class ContentController extends Controller
