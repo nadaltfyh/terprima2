574d311e0a0741d29977eabafa6ce54a8f062779
nadaltfyh <nadalutfiyah1@gmail.com>
2025-05-19 17:51:34 +0700

    favicon2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - TerPRIMA CMS</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo_web.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
